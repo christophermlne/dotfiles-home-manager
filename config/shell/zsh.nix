51fdc9fd488638f1e2e40f315791fb5f2c267c9a
Christopher Milne <christopher.milne@gmail.com>
2021-11-22 13:05:58 -0500

    fix git info in shell prompt

--- config/shell/zsh.nix
+++ config/shell/zsh.nix
@@ -12,11 +12,11 @@ in {
   config = mkIf cfg.enable {
     programs.zsh = {
       enable = true;
-      oh-my-zsh = {
-        enable = true;
-        plugins = [ "git" ];
-        theme = "minimal";
-      };
+      # oh-my-zsh = {
+      #   enable = true;
+      #   plugins = [ "git" ];
+      #   # theme = "minimal";
+      # };
       prezto = {
         enable = true;
         editor.keymap = "vi";
@@ -27,12 +27,24 @@ in {
         export EDITOR=vim
         export VISUAL=vim
         . ~/.nix-profile/etc/profile.d/nix.sh
+
+        function git_info () {
+          if [ -n "$(git rev-parse --git-dir 2> /dev/null)" ]; then
+            echo "($(git rev-parse --abbrev-ref HEAD))"
+          fi
+        }
+        # FIXME: this is overidden because `skwp prompt git_info` isn't a thing.
+        # should just use another definition or copy skwp source and make the following change
+        export PS1="%F{135}%n%f@%F{166}%m%f %F{118}%~%f \$(git_info) \$ "
       '';
 
       shellAliases = {
         vi = "vim";
-	      gs = "git status";
+        gd = "git diff";
+        gb = "git branch";
+        gs = "git status";
      	  gco = "git checkout";
+        fvim = "vim $(fzf)";
       };
 
       enableSyntaxHighlighting = true;
